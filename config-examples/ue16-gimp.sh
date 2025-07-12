a41745708cb003a370b42be96f9c1fd4c38dffbb
elluisian <elluisian@yandex.com>
2025-07-12 02:18:44 +0200

    DevImprovement: Improved GIMP UE16 config
    - Inverted actions of button 1 and 2;
    - Removed "pencil", as I don't use it;
    - Changed location of "Eraser";
    - Added toggling instruments and full screen;
    - Changed location of Rectangle selection and move objects;
    - Added Ctrl and Shift: this way they can be used with scroll click in
      order to zoom (with Ctrl) and rotate (with Shift);
    - Added new wheel action movement: undo/redo operation;
    - Changed default action of key 9 of ue16 so that the ring switch happens;

--- config-examples/ue16-gimp.sh
+++ config-examples/ue16-gimp.sh
@@ -9,43 +9,42 @@
 
 ../ugeeconfig.sh from default-config.xml to gimp-config-ue16.xml set \
 \
-ue16.pen.button2.action_default 'mousek_sclick' \
+ue16.pen.button1.action_default 'mousek_sclick' \
+ue16.pen.button2.action_default 'mousek_rclick' \
 \
 ue16.tablet.key1.action_custom.label 'Paintbrush' \
 ue16.tablet.key1.action_custom.action 'keys(p)' \
 ue16.tablet.key1.action_custom.enabled 'true' \
 \
-ue16.tablet.key2.action_custom.label 'Pencil' \
-ue16.tablet.key2.action_custom.action 'keys(n)' \
+ue16.tablet.key2.action_custom.label 'Eraser' \
+ue16.tablet.key2.action_custom.action 'keys(shift+e)' \
 ue16.tablet.key2.action_custom.enabled 'true' \
 \
-ue16.tablet.key3.action_custom.label 'Lower zoom' \
-ue16.tablet.key3.action_custom.action 'keys(minus)' \
+ue16.tablet.key3.action_custom.label 'Toggle instruments' \
+ue16.tablet.key3.action_custom.action 'keys(tab)' \
 ue16.tablet.key3.action_custom.enabled 'true' \
 \
-ue16.tablet.key4.action_custom.label 'Increase zoom' \
-ue16.tablet.key4.action_custom.action 'keys(plus)' \
+ue16.tablet.key4.action_custom.label 'Toggle fullscreen' \
+ue16.tablet.key4.action_custom.action 'keys(f11)' \
 ue16.tablet.key4.action_custom.enabled 'true' \
 \
-ue16.tablet.key5.action_custom.label 'Copy' \
-ue16.tablet.key5.action_custom.action 'keys(ctrl+c)' \
+ue16.tablet.key5.action_custom.label 'Rectangle selection' \
+ue16.tablet.key5.action_custom.action 'keys(r)' \
 ue16.tablet.key5.action_custom.enabled 'true' \
 \
-ue16.tablet.key6.action_custom.label 'Paste' \
-ue16.tablet.key6.action_custom.action 'keys(ctrl+v)' \
+ue16.tablet.key6.action_custom.label 'Move objects' \
+ue16.tablet.key6.action_custom.action 'keys(m)' \
 ue16.tablet.key6.action_custom.enabled 'true' \
 \
-ue16.tablet.key7.action_custom.label 'Move objects' \
-ue16.tablet.key7.action_custom.action 'keys(m)' \
+ue16.tablet.key7.action_custom.label 'Ctrl' \
+ue16.tablet.key7.action_custom.action 'keys(ctrl)' \
 ue16.tablet.key7.action_custom.enabled 'true' \
 \
-ue16.tablet.key8.action_custom.label 'Rectangular selection' \
-ue16.tablet.key8.action_custom.action 'keys(r)' \
+ue16.tablet.key8.action_custom.label 'Shift' \
+ue16.tablet.key8.action_custom.action 'keys(shift)' \
 ue16.tablet.key8.action_custom.enabled 'true' \
 \
-ue16.tablet.key9.action_custom.label 'Eraser' \
-ue16.tablet.key9.action_custom.action 'keys(shift+e)' \
-ue16.tablet.key9.action_custom.enabled 'true' \
+ue16.tablet.key9.action_default 'funct_swring1' \
 \
 ue16.tablet.r.selected_wheel 1 \
 \
@@ -54,4 +53,11 @@ ue16.tablet.r.wheel_movement1.label 'Increase/decrease brush size' \
 ue16.tablet.r.wheel_movement1.ccw_label 'Decrease' \
 ue16.tablet.r.wheel_movement1.cw_label 'Increase' \
 ue16.tablet.r.wheel_movement1.ccw_action 'keys(altgr+egrave)' \
-ue16.tablet.r.wheel_movement1.cw_action 'keys(altgr+plus)'
+ue16.tablet.r.wheel_movement1.cw_action 'keys(altgr+plus)' \
+\
+ue16.tablet.r.wheel_movement2.usage 'usg_custom' \
+ue16.tablet.r.wheel_movement2.label 'Undo/Redo' \
+ue16.tablet.r.wheel_movement2.ccw_label 'Undo last operation' \
+ue16.tablet.r.wheel_movement2.cw_label 'Redo last operation' \
+ue16.tablet.r.wheel_movement2.ccw_action 'keys(ctrl+z)' \
+ue16.tablet.r.wheel_movement2.cw_action 'keys(ctrl+y)'
